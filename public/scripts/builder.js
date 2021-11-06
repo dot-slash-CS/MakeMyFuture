82a2926aa2269629db9f60a18e50d97a958a86c1
Nicolep02 <nicolewphan02@gmail.com>
2021-11-05 17:12:11 -0700

    department Menu Event listener

---
+++ public/scripts/builder.js
@@ -0,0 +1,13 @@
+/** 
+ * clientside builder
+ * 
+ * 
+ * @file public/script/builder.js
+ * @authors Nicole Phan,
+ * @version 11/05/2021
+ */
+document.addEventListener("DOMContentLoaded", (evt)=>{
+    department = document.getElementById("department-menu")
+    department.addEventListener("change", (evt)=>{console.log("user selected the menu")})
+})
+
